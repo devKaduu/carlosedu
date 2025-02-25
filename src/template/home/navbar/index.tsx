d2f8df5a4727c9eb6043f92e1a824fa90c59a2f6
Carlos Eduardo <kadufenix10@gmail.com>
2025-02-25 12:08:44 -0300

    feat: add download my cv in navbar

--- src/template/home/navbar/index.tsx
+++ src/template/home/navbar/index.tsx
@@ -4,6 +4,7 @@ import { useLenis } from "lenis/react";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { GoHome, GoFile } from "react-icons/go";
+import { motion } from "motion/react";
 
 const tabs = [
   {
@@ -49,9 +50,7 @@ export const Navbar = () => {
       <p className="text-2xl">carlosedu</p>
 
       <div
-        className={`absolute h-8 transition-all duration-300 ease-out bg-[#ffffff1a] rounded-full  ${
-          hoveredIndex === 0 ? "h-11" : "h-8"
-        }`}
+        className={`absolute h-8 transition-all duration-300 ease-out bg-[#ffffff1a] rounded-full`}
         style={{
           ...hoverStyle,
           opacity: hoveredIndex !== null ? 1 : 0,
@@ -77,13 +76,40 @@ export const Navbar = () => {
           </Link>
         ))}
       </ul>
-      <div className="flex items-center">
+      <div className="flex items-center relative">
         <button className="text-base bg-bgButtonNavbar py-3 px-5 rounded-button font-medium">
           <a href="https://www.linkedin.com/in/carlos-eduardo-sousa/">Hire me</a>
         </button>
         <button className="y-3 px-3 rounded-full">
           <a href="./documents/cv/cv-carlos-eduardo-sousa.pdf" target="_blank">
             <GoFile size={22} />
+          </a>
+
+          <a className="absolute w-full top-12" href="./documents/cv/cv-carlos-eduardo-sousa.pdf" target="_blank">
+            <motion.p
+              className="text-sm opacity-50 italic"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 0.5 }}
+              transition={{
+                duration: 1.25,
+                ease: "easeInOut",
+              }}
+            >
+              Download my cv
+            </motion.p>
+            <svg viewBox="0 0 286 73" fill="none" className="absolute -left-0 -right-2 -top-3  bottom-0">
+              <motion.path
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{
+                  duration: 1.25,
+                  ease: "easeInOut",
+                }}
+                d="M142.293 1C106.854 16.8908 6.08202 7.17705 1.23654 43.3756C-2.10604 68.3466 29.5633 73.2652 122.688 71.7518C215.814 70.2384 316.298 70.689 275.761 38.0785C230.14 1.37835 97.0503 24.4575 52.9384 1"
+                stroke="#403f42"
+                strokeWidth="3"
+              />
+            </svg>
           </a>
         </button>
       </div>
