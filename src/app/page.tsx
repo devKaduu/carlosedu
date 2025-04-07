b47626f4f9b918bc2492381ca3e4ea4ce63f6d9c
Carlos Eduardo <kadufenix10@gmail.com>
2025-04-07 15:20:57 -0300

    feat: add new cv in navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "carlosedu",
   description:
-    "Portfolio of Carlos Eduardo, a fullstack developer specialized in React, TypeScript, and interactive animations with GSAP and Framer Motion.",
+    "Portfolio of Carlos Eduardo, a fullstack developer specialized in React, React Native, NextJs, TypeScript, and interactive animations with GSAP and Framer Motion.",
 };
 
 export default function Home() {
